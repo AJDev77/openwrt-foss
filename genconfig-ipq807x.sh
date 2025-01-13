52d654f1825ff6c9ec7051a36506a86d67b2fd8f
AJDev77 <AJDev77@users.noreply.github.com>
2025-01-13 16:41:52 -0500

    Update genconfig-ipq807x.sh

--- genconfig-ipq807x.sh
+++ genconfig-ipq807x.sh
@@ -13,6 +13,21 @@ echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300="" >
 #add luci
 echo CONFIG_PACKAGE_luci=y >> .config
 echo CONFIG_TARGET_OPTIMIZATION="-O2 -pipe -mcpu=cortex-a53+crc+crypto" >> .config
+echo CONFIG_DEVEL=y >> .config
+echo CONFIG_EXPERIMENTAL=y >> .config
+echo CONFIG_TOOLCHAINOPTS=y >> .config
+echo CONFIG_TARGET_OPTIONS=y >> .config
+echo CONFIG_USE_GC_SECTIONS=y >> .config
+
+
+# Kernel Config
+echo CONFIG_COLLECT_KERNEL_DEBUG=y >> .config
+echo CONFIG_KERNEL_PERF_EVENTS=y >> .config
+echo CONFIG_KERNEL_DYNAMIC_DEBUG=y >> .config
+echo CONFIG_KERNEL_ARM_PMU=y >> .config
+echo CONFIG_KERNEL_ARM_PMUV3=y >> .config
+echo CONFIG_KERNEL_PREEMPT_NONE=y >> .config
+echo CONFIG_KERNEL_PREEMPT_NONE_BUILD=y >> .config
 make defconfig
 
 #skip xdp
