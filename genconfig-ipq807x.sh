807df63678887075c1917bc4954b0295789aa599
AJDev77 <AJDev77@users.noreply.github.com>
2025-01-14 17:32:43 -0500

    Update genconfig-ipq807x.sh

--- genconfig-ipq807x.sh
+++ genconfig-ipq807x.sh
@@ -2,17 +2,16 @@
 
 arch=qualcommax
 target=ipq807x
-version=releases/24.10.0-rc4
+version=releases/24.10.0-rc5
 wget https://downloads.openwrt.org/${version}/targets/${arch}/${target}/config.buildinfo -O config.buildinfo
 cat config.buildinfo | grep -v CONFIG_TARGET_DEVICE_ | grep -v CONFIG_TARGET_ALL_PROFILES | grep -v CONFIG_TARGET_MULTI_PROFILE > .config
-echo CONFIG_TARGET_ALL_PROFILES=n >> .config
-echo CONFIG_TARGET_MULTI_PROFILE=n >> .config
 echo CONFIG_TARGET_DEVICE_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
-echo CONFIG_TARGET_DEVICE_PACKAGES_qualcommax_ipq807x_DEVICE_linksys_mx4300="" >> .config
+echo CONFIG_TARGET_qualcommax_ipq807x_DEVICE_linksys_mx4300=y >> .config
+echo CONFIG_TARGET_PROFILE="DEVICE_linksys_mx4300" >> .config
 
 #add luci
 echo CONFIG_PACKAGE_luci=y >> .config
-echo CONFIG_TARGET_OPTIMIZATION="-O2 -pipe -mcpu=cortex-a53+crc+crypto" >> .config
+echo CONFIG_TARGET_OPTIMIZATION='-O2 -pipe -mcpu=cortex-a53+crc+crypto' >> .config
 echo CONFIG_DEVEL=y >> .config
 echo CONFIG_EXPERIMENTAL=y >> .config
 echo CONFIG_TOOLCHAINOPTS=y >> .config
@@ -28,6 +27,7 @@ echo CONFIG_KERNEL_ARM_PMU=y >> .config
 echo CONFIG_KERNEL_ARM_PMUV3=y >> .config
 echo CONFIG_KERNEL_PREEMPT_NONE=y >> .config
 echo CONFIG_KERNEL_PREEMPT_NONE_BUILD=y >> .config
+cat .config
 make defconfig
 
 #skip xdp
